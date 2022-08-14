9ebbd2d7e966df8cc81f9b5e0202962985d8e55e
nullclear <1206134796@qq.com>
2022-08-14 21:48:50 +0800

    docs(过滤器): 过滤器顺序

--- src/main/java/dev/yxy/SpringSecurityDemoApplication.java
+++ src/main/java/dev/yxy/SpringSecurityDemoApplication.java
@@ -4,7 +4,16 @@ import dev.yxy.property.RedisProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+import org.springframework.session.web.http.SessionRepositoryFilter;
 
+/**
+ * 过滤器顺序
+ * {@link SessionRepositoryFilter}
+ * {@link SecurityContextPersistenceFilter}
+ * {@link UsernamePasswordAuthenticationFilter}
+ */
 @EnableConfigurationProperties(value = {RedisProperty.class})
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
