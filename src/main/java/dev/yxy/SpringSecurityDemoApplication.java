9baf4675b1bbc4a40c36665974fa35a3fdd077a3
nullclear <1206134796@qq.com>
2022-09-17 20:31:12 +0800

    docs(源码阅读): Spring Security 责任链模式

--- src/main/java/dev/yxy/SpringSecurityDemoApplication.java
+++ src/main/java/dev/yxy/SpringSecurityDemoApplication.java
@@ -4,6 +4,7 @@ import dev.yxy.property.RedisProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.session.web.http.SessionRepositoryFilter;
@@ -13,7 +14,12 @@ import org.springframework.session.web.http.SessionRepositoryFilter;
  * {@link SessionRepositoryFilter}
  * {@link SecurityContextPersistenceFilter}
  * {@link UsernamePasswordAuthenticationFilter}
+ *
+ * NOTE - 2022/09/17 责任链模式
+ * Spring Security 内部的虚拟过滤器链 {@link FilterChainProxy.VirtualFilterChain}
+ * 在原始过滤器链中建立一个虚拟过滤器链，执行完毕继续执行原始过滤器链
  */
+@SuppressWarnings("JavadocReference")
 @EnableConfigurationProperties(value = {RedisProperty.class})
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
